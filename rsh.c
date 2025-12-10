80b75b2e5fc4f5339cb554d3d225a7f9fdcb8705
aladdin2121 <aladdinbouk@gmail.com>
2025-12-09 21:29:17 -0700

    6th attempt

--- rsh.c
+++ rsh.c
@@ -144,8 +144,12 @@ int main(int argc, char **argv) {
 
     // TODO:
     // create the message listener thread
-
+   
     pthread_t listener_thread;
+    if (pthread_create(&listener_thread, NULL, messageListener, NULL) != 0) {
+        perror("Failed to create message listener thread");
+        exit(EXIT_FAILURE);
+    }
 
     //detach the thread
     pthread_detach(listener_thread); 
